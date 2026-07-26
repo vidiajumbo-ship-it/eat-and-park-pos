8d1398bfb26c70668cc75f60d2f9b11163a69a5a
eat-park-ara <vidiajumbo@gmail.com>
2026-07-26 19:30:41 +0530

    Fix build dependencies and vite setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"; // <-- Yeh zaroori hai database ke liye
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC67vNT4nVAHm31Dohgjl4K5lJ5EQS7pe4",
   authDomain: "enpmenu.firebaseapp.com",
@@ -19,3 +17,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// <-- Yeh line add karein taaki baaki files mein db use kar sakein
+export const db = getFirestore(app);
